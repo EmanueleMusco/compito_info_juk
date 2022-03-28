e3063aa79bcc858c59fd7c4e89e33ff4f27ca2be
Emanuele <92219909+morefearless@users.noreply.github.com>
2022-03-28 19:12:20 +0200

    Add files via upload

---
+++ JukeBoxMusco/JukeboxMusco/aggiungi.php
@@ -0,0 +1,39 @@
+<?php 
+include 'connessione.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Jukebox</title>
+</head>
+<body>
+<h1 align="center"> <a href="index.php">JUKEBOX</a> </h1>
+<div class="container">
+<div class="grid-container">
+    <div class="grid-item" style="color: rgb(236, 100, 85);"> <a href="aggiungi.php"><img src="icone/plus.png" ><br>Aggiungi cantante/canzone</a></div>
+    <div class="grid-item"><a href="cerca.php"><img src="icone/search.png" ><br>Cerca cantante/canzone</a></div>
+    <div class="grid-item"><a href="elimina.php"><img src="icone/bin.png" ><br>Cancella cantante/canzone</a></div>
+    <div class="grid-item"><a href="aggiorna.php"><img src="icone/refresh.png" ><br>Aggiorna cantante/canzone</a></div>
+</div>
+
+<!-- div di spaziatura-->
+<div class="spazio"></div>
+<div class="cantante">
+<a href="inserisciCantante.php"> Inserisci cantante</a>
+</div>
+<div class="spazio"></div>
+<div class="canzone">
+<a href="inserisciCanzone.php">Inserisci canzone</a>
+</div>
+<div class="spazio"></div>
+
+
+</body>
+</html>
+
+
+
